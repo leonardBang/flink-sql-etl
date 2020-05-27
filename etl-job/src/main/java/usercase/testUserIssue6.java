c553d85fc94b8ae59419e9035900cad6d1c0a72e
Leonard Xu <xbjtdcq@gmail.com>
2020-05-27 09:59:45 +0800

    add user case

--- etl-job/src/main/java/usercase/testUserIssue6.java
+++ etl-job/src/main/java/usercase/testUserIssue6.java
@@ -5,23 +5,15 @@ import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.table.descriptors.Json;
 import org.apache.flink.table.descriptors.Kafka;
-import org.apache.flink.table.descriptors.Rowtime;
 import org.apache.flink.table.descriptors.Schema;
+import org.apache.flink.types.Row;
+
+import static org.apache.flink.table.api.DataTypes.FIELD;
+import static org.apache.flink.table.api.DataTypes.INT;
+import static org.apache.flink.table.api.DataTypes.ROW;
 import static org.apache.flink.table.api.DataTypes.STRING;
 
 public class testUserIssue6 {
-
-    private static String hbaseSourceDDL = "CREATE TABLE country (\n" +
-            "  rowkey VARCHAR,\n" +
-            "  f1 ROW<country_id INT, country_name VARCHAR, country_name_cn VARCHAR, currency VARCHAR, region_name VARCHAR> \n" +
-            " " +
-            ") WITH (\n" +
-            "    'connector.type' = 'hbase',\n" +
-            "    'connector.version' = '1.4.3',\n" +
-            "    'connector.table-name' = 'country',\n" +
-            "    'connector.zookeeper.quorum' = 'localhost:2182',\n" +
-            "    'connector.zookeeper.znode.parent' = '/hbase' " +
-            ")";
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -35,50 +27,25 @@ public class testUserIssue6 {
         tableEnvironment.connect(
             new Kafka()
                 .topic("test-json")
-                .version("0.11")
+                .version("0.10")
                 .property("bootstrap.servers", "localhost:9092")
+                .property("zookeeper.connect", "localhost:2181")
                 .property("group.id", "testGroup"))
             .withFormat(new Json())
             .withSchema(new Schema()
-                .field("table", STRING())
-                .field("database", STRING())
-                .field("gmt_time", STRING())
-                // process time 声明： descriptor API 生成的 处理时间 列名为proctime
-                .proctime()
-                // event time 声明：gmt_time为schema中的列, watermark 的delay为5s
-                .rowtime(new Rowtime().timestampsFromField("gmt_time").watermarksPeriodicBounded(5 * 1000L))
-
-         )
-//            .withSchema(new Schema()
-//                .field("data", ROW(
-//                    FIELD("reference_id", STRING()),
-//                    FIELD("transaction_type", INT()),
-//                    FIELD("merchant_id", INT()),
-//                    FIELD("status", INT()),
-//                    FIELD("create_time", INT())
-//                    )
-//                ).rowtime(new Rowtime().timestampsFromField("xxx"))
-//            )
+                .field("general",STRING())
+                 .field("data", ROW(
+                    FIELD("reference_id", STRING()),
+                    FIELD("transaction_type", INT()),
+                    FIELD("merchant_id", INT()),
+                    FIELD("status", INT()),
+                    FIELD("create_time", INT())
+                    )
+                )
+            )
             .createTemporaryTable("KafkaSource");
-
-
-
-        //{
-        //   "database":"main_db",
-        //   "maxwell_ts":1590416550358000,
-        //   "table":"transaction_tab",
-        //   "data":{
-        //       "transaction_sn":"8888",
-        //       "parent_id":0,
-        //       "user_id":333,
-        //       "amount":555,
-        //       "reference_id":"666",
-        //       "status":3,
-        //       "transaction_type":3,
-        //       "merchant_id":2,
-        //       "update_time":1590416550,
-        //       "create_time":1590416550
-        //   }
-        //}
+            tableEnvironment.toAppendStream(tableEnvironment.sqlQuery("select general, reference_id, data.reference_id from KafkaSource"), Row.class)
+            .print();
+           tableEnvironment.execute("case6");
     }
 }
