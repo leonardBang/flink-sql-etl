64cf1295acf2f131c84b8d314eaea1753c13375a
Leonard Xu <xbjtdcq@gmail.com>
2020-06-05 22:34:03 +0800

    add HBase 1.11 sql

---
+++ etl-job/src/main/resources/job-sql-1.11/kafka2hbase/UnboundedKafkaJoinHbase2Hbase.sql
@@ -0,0 +1,72 @@
+CREATE TABLE orders (
+  order_id STRING,
+  item    STRING,
+  currency STRING,
+  amount INT,
+  order_time TIMESTAMP(3),
+  proc_time as PROCTIME(),
+  amount_kg as amount * 1000,
+  ts as order_time + INTERVAL '1' SECOND,
+  WATERMARK FOR order_time AS order_time
+) WITH (
+  'connector' = 'kafka',
+  'topic' = 'flink_orders2',
+  'properties.zookeeper.connect' = 'localhost:2181',
+  'properties.bootstrap.servers' = 'localhost:9092',
+  'properties.group.id' = 'testGroup3',
+  'scan.startup.mode' = 'earliest-offset',
+  'format' = 'json'
+);
+
+CREATE TABLE currency (
+  currency_id BIGINT,
+  currency_name STRING,
+  rate DOUBLE,
+  currency_time TIMESTAMP(3),
+  country STRING,
+  timestamp9 TIMESTAMP(3),
+  time9 TIME(3),
+  gdp DECIMAL(38, 18)
+) WITH (
+   'connector' = 'jdbc',
+   'url' = 'jdbc:mysql://localhost:3306/test',
+   'username' = 'root',
+   'table-name' = 'currency',
+   'password' = '',
+   'driver' = 'com.mysql.jdbc.Driver',
+   'lookup.cache.max-rows' = '500',
+   'lookup.cache.ttl' = '10s',
+   'lookup.max-retries' = '3');
+
+CREATE TABLE country (
+  rowkey VARCHAR,
+  f1 ROW<country_id INT, country_name VARCHAR, country_name_cn VARCHAR, currency VARCHAR, region_name VARCHAR>
+  ,f2 ROW<record_timestamp3 TIMESTAMP(3), record_timestamp9 TIMESTAMP(3), time3 TIME(3), time9 TIME(3), gdp DECIMAL(38,18)>) WITH (
+    'connector' = 'hbase-1.4',
+    'table-name' = 'country',
+    'zookeeper.quorum' = 'localhost:2182',
+    'zookeeper.znode.parent' = '/hbase' );
+
+CREATE TABLE gmv (
+  rowkey VARCHAR,
+  f1 ROW<log_ts VARCHAR,item VARCHAR,country_name VARCHAR>
+) WITH (
+    'connector' = 'hbase-1.4',
+    'table-name' = 'gmv',
+    'zookeeper.quorum' = 'localhost:2182',
+    'zookeeper.znode.parent' = '/hbase',
+    'sink.buffer-flush.max-size' = '10mb',
+    'sink.buffer-flush.max-rows' = '1000',
+    'sink.buffer-flush.interval' = '2s' );
+
+ insert into gmv
+ select  rowkey, ROW(max(ts), max(item), max(country_name)) as f1
+ from (select concat(cast(o.ts as VARCHAR), '_', item, '_', co.f1.country_name) as rowkey,
+ cast(o.ts as VARCHAR) as ts, o.item as item, co.f1.country_name as country_name
+ from orders as o
+ left outer join currency FOR SYSTEM_TIME AS OF o.proc_time c
+ on o.currency = c.currency_name
+ left outer join country FOR SYSTEM_TIME AS OF o.proc_time co
+ on c.country = co.rowkey
+) a group by rowkey
+
